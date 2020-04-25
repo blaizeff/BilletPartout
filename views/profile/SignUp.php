105e0832f92194bcf55b2e4c798db85de91c2ddd
Maux-Antoine <63753614+Maux-Antoine@users.noreply.github.com>
2020-04-25 12:28:11 -0400

    css change

--- views/profile/SignUp.php
+++ views/profile/SignUp.php
@@ -10,20 +10,9 @@
             <div class="col-12 col-md-5 col-lg-4 py-8 py-md-11" style="padding-top:240px">
                 <h1 class="title">Créer un compte</h1>
                 <div style="color:red;text-align:center"><?php echo User::errorMessage(); ?></div>
-<<<<<<< HEAD
-                <input type="text" class="form-control form-control-lg customInput" name="email" placeholder="Entrez votre adresse courriel" required>
-<<<<<<< HEAD
-                <input type="password" class="form-control form-control-lg customInput loginInput" name="password" placeholder="Entrez votre mot de passe" required>
-                <input type="password" class="form-control form-control-lg customInput loginInput" name="confirm" placeholder="Confirmez votre mot de passe" required>
-=======
                 <input type="text" class="form-control form-control-lg customInput" name="email" placeholder="Entrez votre adresse courriel">
                 <input type="password" class="form-control form-control-lg customInput" name="password" placeholder="Entrez votre mot de passe">
                 <input type="password" class="form-control form-control-lg customInput" name="confirm" placeholder="Confirmez votre mot de passe">
->>>>>>> fbf2fb03c586af0dd4cc3fb27bf310ec9cc1bf58
-=======
-                <input type="password" class="form-control form-control-lg customInput" name="password" placeholder="Entrez votre mot de passe" required>
-                <input type="password" class="form-control form-control-lg customInput" name="confirm" placeholder="Confirmez votre mot de passe" required>
->>>>>>> parent of 148d76d... css change
                 <button type="submit" name="submit" class="btn btn-lg btn-block btn-primary green">Se connecter</button>
 
                 <div class="text-center">
